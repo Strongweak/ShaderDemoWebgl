a9e78a794774069e6e1389dc8e08976c3d6f23b7
Strongest <111754516+Strongweak@users.noreply.github.com>
2024-09-17 00:24:13 +0700

    Update
    More stat counter
    - Weapon
    - Quake movement
    - Optimize
    - Beach shader

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Shader demo-0.1.0";
+const contentToCache = [
+    "Build/ShaderDemoWebgl.loader.js",
+    "Build/ShaderDemoWebgl.framework.js.unityweb",
+    "Build/ShaderDemoWebgl.data.unityweb",
+    "Build/ShaderDemoWebgl.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
